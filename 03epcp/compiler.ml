957b94f0487a4d9ad0087c4d4dcfa9645bb133a1
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2025-02-24 18:32:16 +0000

    Add epcp compiler (just integers again, but with a lexer and parser).

---
+++ 03epcp/compiler.ml
@@ -0,0 +1,20 @@
+open Printf
+
+(* A very sophisticated compiler - insert the given integer into the mov
+instruction at the correct place *)
+let compile (program : int64) : string =
+  sprintf "
+section .text
+global our_code_starts_here
+our_code_starts_here:
+  mov RAX, %Ld
+  ret\n" program;;
+
+(* Some OCaml boilerplate for reading files and command-line arguments *)
+let () =
+  let input_file = (open_in (Sys.argv.(1))) in
+  let lexbuf = Lexing.from_channel input_file in
+  let input_program = Parser.prog Lexer.read lexbuf in
+  close_in input_file;
+  let program = (compile input_program) in
+  printf "%s\n" program
